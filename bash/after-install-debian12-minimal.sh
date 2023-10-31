024bbd227348ef4d5bcc07a84b83c523a36f4d64
anttioll <anttiollikainen@kamk.fi>
2023-10-31 17:05:56 +0200

    Säätöä

--- bash/after-install-debian12-minimal.sh
+++ bash/after-install-debian12-minimal.sh
@@ -2,28 +2,43 @@
 # Debian 12 Bookworm minimaalisen asennuksen jälkeinen skripti
 # 10/2023 Antti Ollikainen
 
-sed -i "1d,2d" /etc/apt/sources.list
+#sed -i "1d,2d" /etc/apt/sources.list
 
 apt update -q -y && apt upgrade -q -y
 
-apt install -q -y man vim lsof
+apt install -q -y man vim lsof dnsutils iptables-persistent chrony
 
-echo "alias ll='ls -lah --color=auto'" > /etc/profile.d/alias.sh
-echo "alias rm='rm -i'" >> /etc/profile.d/alias.sh
-echo "alias mv='mv -i'" >> /etc/profile.d/alias.sh
-echo "alias cp='cp -i'" >> /etc/profile.d/alias.sh
+ALIAS_SH=/etc/profile.d/alias.sh
+echo "alias ll='ls -lah --color=auto'" > $ALIAS_SH
+echo "alias rm='rm -i'" >> $ALIAS_SH
+echo "alias mv='mv -i'" >> $ALIAS_SH
+echo "alias cp='cp -i'" >> $ALIAS_SH
 
-echo "set nu!" >> /etc/vim/vimrc
-echo "colorscheme desert" >> /etc/vim/vimrc
-echo "set expandtab" >> /etc/vim/vimrc
-echo "set tabstop=2" >> /etc/vim/vimrc
-echo "set shiftwidth=2" >> /etc/vim/vimrc
-echo "set autoindent" >> /etc/vim/vimrc
+VIM_CONF=/etc/vim/vimrc
+echo "set nu!" >> $VIM_CONF
+echo "colorscheme desert" >> $VIM_CONF
+echo "set expandtab" >> $VIM_CONF
+echo "set tabstop=2" >> $VIM_CONF
+echo "set shiftwidth=2" >> $VIM_CONF
+echo "set autoindent" >> $VIM_CONF
 
-sed -i "s/#\?LoginGraceTime.*/LoginGraceTime 1m/g" /etc/ssh/sshd_config
-sed -i "s/#\?PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
-sed -i "s/#\?MaxAuthTries.*/MaxAuthTries 3/g" /etc/ssh/sshd_config
-sed -i "s/#\?PermitEmptyPasswords.*/PermitEmptyPasswords no/g" /etc/ssh/sshd_config
-sed -i "s/#\?AllowTcpForwarding.*/AllowTcpForwarding no/g" /etc/ssh/sshd/config
-sed -i "s/#\?X11Forwarding.*/X11Forwarding no/g" /etc/ssh/sshd_config
-sed -i "s/#\?PermitTunnel.*/PermitTunnel no/g" /etc/ssh/sshd_config
+CHRONY_CONF=/etc/chrony/chrony.conf
+echo "server 10.10.10.5" > $CHRONY_CONF
+echo "server 10.10.10.6" >> $CHRONY_CONF
+echo "driftfile /var/lib/chrony/chrony.drift" >> $CHRONY_CONF
+echo "#log tracking measurements statistics" >> $CHRONY_CONF
+echo "logdir /var/log/chrony" >> $CHRONY_CONF
+echo "maxudpateskew 100.0" >> $CHRONY_CONF
+echo "rtcsync" >> $CHRONY_CONF
+echo "makestep 1 3" >> $CHRONY_CONF
+echo "leapsectz right/UTC" >> $CHRONY_CONF
+echo "cmdport 0" >> $CHRONY_CONF
+
+SSHD_CONF=/etc/ssh/sshd_config
+sed -i "s/#\?LoginGraceTime.*/LoginGraceTime 1m/g" $SSHD_CONF
+sed -i "s/#\?PermitRootLogin.*/PermitRootLogin no/g" $SSHD_CONF
+sed -i "s/#\?MaxAuthTries.*/MaxAuthTries 3/g" $SSHD_CONF
+sed -i "s/#\?PermitEmptyPasswords.*/PermitEmptyPasswords no/g" $SSHD_CONF
+sed -i "s/#\?AllowTcpForwarding.*/AllowTcpForwarding no/g" $SSHD_CONF
+sed -i "s/#\?X11Forwarding.*/X11Forwarding no/g" $SSHD_CONF
+sed -i "s/#\?PermitTunnel.*/PermitTunnel no/g" $SSHD_CONF
